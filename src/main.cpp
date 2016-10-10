df5cda6269e3ec04e3c91ee01e0da15164fb3db9
Unknown <luigytkd@gmail.com>
2016-10-10 15:47:09 +0100

    Added boost stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#include <boost/locale.hpp>
+#include <ctime>
+
 void doSomeCpp11Stuff()
 {
 	std::vector<std::string> names = { "xyzzy", "plugh", "abracadabra" };
@@ -14,11 +17,38 @@ void doSomeCpp11Stuff()
 	std::cout << std::endl;
 }
 
+void doSomeBoostStuff()
+{
+	using namespace boost::locale;
+    using namespace std;
+    generator gen;
+    locale loc=gen(""); 
+    // Create system default locale
+
+    locale::global(loc); 
+    // Make it system global
+    
+    cout.imbue(loc);
+    // Set as default locale for output
+    
+    cout <<format("Today {1,date} at {1,time} we had run our first localization example") % time(0) 
+          <<endl;
+   
+    cout<<"This is how we show numbers in this locale "<<as::number << 103.34 <<endl; 
+    cout<<"This is how we show currency in this locale "<<as::currency << 103.34 <<endl; 
+    cout<<"This is typical date in the locale "<<as::date << std::time(0) <<endl;
+    cout<<"This is typical time in the locale "<<as::time << std::time(0) <<endl;
+    cout<<"This is upper case "<<to_upper("Hello World!")<<endl;
+    cout<<"This is lower case "<<to_lower("Hello World!")<<endl;
+    cout<<"This is title case "<<to_title("Hello World!")<<endl;
+    cout<<"This is fold case "<<fold_case("Hello World!")<<endl;
+}
 
 int main()
 {
 
 	doSomeCpp11Stuff();
+	doSomeBoostStuff();
 	std::cout<<"yeahhhh"<<std::endl;
 	return 0;
 }
